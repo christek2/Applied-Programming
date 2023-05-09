9aa259bd2d608bfc2fe8b7b4b0362ae5b820fe95
Kimball <kimballchristensen@gmail.com>
2023-05-09 16:46:52 -0600

    Worked on practicing Erlang

--- Module2/tasks.erl
+++ Module2/tasks.erl
@@ -1,4 +1,12 @@
 -module(tasks).
--export([hello_world/0]).
+-export([print_my_name/0]).
+-export([print_my_bday/0]).
+-export([handle_message/1]).
 
-hello_world() -> io:fwrite("hello world!").
+print_my_name() -> io:fwrite("My name is Kimball Christensen ").
+print_my_bday() -> io:fwrite("My birthday is 11/14/2023 ").
+
+handle_message({get_user, UserId}) ->
+    {ok, User} = database:get_user(UserId), 
+    io:format("User ~p~n", [User]),
+    {reply, User}.
