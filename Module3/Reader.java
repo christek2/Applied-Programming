7dea39c8a8bc5a31f345e61ac4efa7250db8ff8a
Kimball <kimballchristensen@gmail.com>
2023-06-03 11:49:12 -0600

    Worked on some Java testing

---
+++ Module3/Reader.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Reader {
+    public static void main (String[] args) {
+        System.out.println(populate(1));
+    }
+    public static String populate (int BoyName){
+        String path = "Module3/name2.csv";
+        String line = "";
+        List<String> boyNames = new ArrayList<>();
+        List<String> girlNames = new ArrayList<>();
+        List<String> lastNames = new ArrayList<>();
+        
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            while((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                boyNames.add(parts[0]);
+                girlNames.add(parts[1]);
+                lastNames.add(parts[2]);
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (BoyName == 1) {
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+            String fName = boyNames.get(rnd1.nextInt(1000));
+            String lName = lastNames.get(rnd2.nextInt(1000));
+            return String.format("%s %s", fName, lName);
+        } else if (BoyName == 0){
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+            String fName = girlNames.get(rnd1.nextInt(1000));
+            String lName = lastNames.get(rnd2.nextInt(1000));
+            return String.format("%s %s", fName, lName);
+        } else {
+            return "Error";
+        }
+    }
+}
