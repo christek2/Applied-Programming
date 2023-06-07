d14942ee2b9894cec449e28d2e03f86ae7116d77
christek2 <kimballchristensen@gmail.com>
2023-06-07 12:24:31 -0600

    Worked on getting and removing items from lists

--- Module4/tasks.cpp
+++ Module4/tasks.cpp
@@ -3,26 +3,43 @@
 #include <string>
 #include <list>
 #include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    // std::list<string> greetings = {
-    //     "Ola",
-    //     "Oi mano",
-    //     "Tudo bem?",
-    //     "Como vai",
-    //     "Como estas?",
-    //     "Bom dia"
-    // };
+    list<string> greetings;
 
-    // for (string i : greetings) {
-    //     cout << i << endl;
-    // }
+    greetings.push_back("Ola");
+    greetings.push_back("Oi mano");
+    greetings.push_back("Tudo bem?");
+    greetings.push_back("Como vai?");
+    greetings.push_back("Como estas?");
+    greetings.push_back("Bom dia");
+
+    int removeNum = 0;
+    int randNum = 0;
+    int index = 0;
+    string removeStr;
 
     srand(time(0));
-    int i = (rand() % 10) + 1;
+    randNum = (rand() % (6 - removeNum));
+    // cout << randNum << endl;
+    for (string choice : greetings) {
+        if (index == randNum) {
+            cout << choice << endl;
+            removeStr = choice;
+            break;
+        }
+        index = index + 1;
+    }
 
-    cout << i << endl;
+    greetings.remove(removeStr);
+    removeNum = removeNum + 1;    
+    
+    cout << "" << endl;
+    for (string i : greetings) {
+        cout << i << endl;
+    }
 }
